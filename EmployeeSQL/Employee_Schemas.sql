0a7d33b4f821c652c7912a5dfc444f9e9e511483
sholli0919 <shollis0919@gmail.com>
2023-06-05 22:35:47 -0400

    revised homework files

--- EmployeeSQL/Employee_Schemas.sql
+++ EmployeeSQL/Employee_Schemas.sql
@@ -1,10 +1,10 @@
-DROP TABLE titles;
+DROP TABLE departments;
 
 -- Create the various tabels needed to import the CSV's
 CREATE TABLE "departments" (
     "id" SERIAL   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL UNIQUE,
-    "dept_name" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR (10)  NOT NULL UNIQUE,
+    "dept_name" VARCHAR NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "id"
      )
@@ -16,8 +16,8 @@ SELECT * FROM departments;
 --Add foreign keys to reference other tables
 CREATE TABLE "dept_emp" (
     "id" SERIAL   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT NOT NULL,
+    "dept_no" VARCHAR (10) NOT NULL,
 	FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no"), 
     FOREIGN KEY ("dept_no") REFERENCES "departments" ("dept_no"),
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
@@ -28,9 +28,9 @@ CREATE TABLE "dept_emp" (
 SELECT * FROM dept_emp;
 
 CREATE TABLE "dept_manager" (
-    "id" SERIAL   NOT NULL,
-	"dept_no" VARCHAR   NOT NULL,
-    "emp_no" INT   NOT NULL,
+    "id" SERIAL NOT NULL,
+	"dept_no" VARCHAR (10)  NOT NULL,
+    "emp_no" INT NOT NULL,
     FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no"), 
     FOREIGN KEY ("dept_no") REFERENCES "departments" ("dept_no"),
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
@@ -42,14 +42,14 @@ SELECT * FROM dept_manager;
 
 
 CREATE TABLE "employees" (
-    "id" SERIAL   NOT NULL,
+    "id" SERIAL NOT NULL,
     "emp_no" INT NOT NULL UNIQUE,
-    "emp_title" VARCHAR   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" VARCHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
+    "emp_title" VARCHAR (10) NOT NULL,
+    "birth_date" DATE NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "sex" VARCHAR NOT NULL,
+    "hire_date" DATE NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "id"
      ),
@@ -59,9 +59,9 @@ CREATE TABLE "employees" (
 SELECT * FROM employees;
 
 CREATE TABLE "salaries" (
-    "id" SERIAL   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
+    "id" SERIAL NOT NULL,
+    "emp_no" INT NOT NULL,
+    "salary" INT NOT NULL,
 	FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no"), 
     CONSTRAINT "pk_salaries" PRIMARY KEY (
         "id"
@@ -72,7 +72,7 @@ SELECT * FROM salaries;
 
 CREATE TABLE "titles" (
     "id" SERIAL PRIMARY KEY NOT NULL,
-    "title_id" VARCHAR   NOT NULL UNIQUE,
-    "title" VARCHAR   NOT NULL
+    "title_id" VARCHAR (10) NOT NULL UNIQUE,
+    "title" VARCHAR NOT NULL
 );
 SELECT * FROM titles;
